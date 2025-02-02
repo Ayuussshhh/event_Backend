117fad3c757d1ca0cf52957ba6e5a182cadd68e2
Ayuussshhh <149790144+Ayuussshhh@users.noreply.github.com>
2025-02-02 11:22:48 +0530

    Delete routes/checkoutRoutes.js

--- routes/checkoutRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const checkoutController = require('../controllers/checkoutController');
-
-// Handle the checkout POST request
-router.post('/checkout', checkoutController.checkout);
-
-module.exports = router;
