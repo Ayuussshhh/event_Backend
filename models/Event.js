a99ba3fc6675b170ff16f940b94f4d7b7f0192df
Ayuussshhh <149790144+Ayuussshhh@users.noreply.github.com>
2025-02-02 11:20:35 +0530

    Update and rename event.js to Event.js

---
+++ models/Event.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    ticketPrice: {
+      type: Number,
+      required: true,
+    },
+    availableTickets: {
+      type: Number,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Event = mongoose.model('Event', eventSchema);
