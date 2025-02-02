d05743c1f418011e24c280dbd9b58f3b030a9b3c
Ayuussshhh <149790144+Ayuussshhh@users.noreply.github.com>
2025-02-02 11:21:03 +0530

    Update and rename user.js to User.js

---
+++ models/User.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true, // Allows null values for uniqueness
+    },
+    githubId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    profilePic: {
+      type: String,
+      default: '',
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model('User', userSchema); 
+export default User;
